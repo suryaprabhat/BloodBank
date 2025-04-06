226181511c75642b1863baed13f8adabc2dab8eb
suryaprabhat <maddalasuryaprabhat@gmail.com>
2025-04-06 17:14:20 +0530

    v

--- server/index.js
+++ server/index.js
@@ -9,9 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // ✅ Fixed CORS syntax
-app.use(cors({
-  origin: ["http://localhost:5173", "https://blood-bank-tau-plum.vercel.app"],
-}));
+app.use(cors());
 
 app.use(express.json());
 
