e38db64dca9e52934358061e26912534c5cd399d
suryaprabhat <maddalasuryaprabhat@gmail.com>
2025-04-27 16:31:11 +0530

    fix

--- frontend/src/pages/maptest/MapTesting.tsx
+++ frontend/src/pages/maptest/MapTesting.tsx
@@ -1,10 +1,13 @@
 import ArcGISMap from "../../components/leaflet/LeafletMap";
 
 function Map() {
+    const handleSelectLocation = (location: { lat: number; lng: number; placeName: string }) => {
+        console.log("Selected location:", location);
+    };
 
     return (
         <>
-            <ArcGISMap />
+            <ArcGISMap onSelectLocation={handleSelectLocation} />
         </>
     )
 }
